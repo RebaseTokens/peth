3da3a95d724dc0cf1ded3710588364599585ba62
RebaseTokens <rebasetwt@gmail.com>
2021-12-02 15:50:21 +0100

    a

--- yoyo.py
+++ yoyo.py
@@ -6,20 +6,26 @@ s = sched.scheduler(time.time, time.sleep)
 
 
 def pumpeth(sc):
-    with open('/root/Desktop/rebase/pumpeth.json', 'r') as file :
+    print('Enter')
+    with open('/root/Desktop/peth/peth.json', 'r') as file :
       filedata = file.read()
 
     filedata = filedata.replace(']\n[', ',')
     filedata = filedata.replace('][', ',')
 
-    with open('/root/Desktop/rebase/pumpeth.json', 'w') as file:
+    with open('/root/Desktop/peth/peth.json', 'w') as file:
       file.write(filedata)
   
-    s.enter(30, 1, commit, (s,))
+    s.enter(5, 1, commit, (s,))
     
 
 def commit(sc):
     print('Commit')
-    subprocess.call(['/root/Desktop/rebase/autocommit.sh'])
+    subprocess.call(['/root/Desktop/peth/auto.sh'])
+    print('Done')
   
     s.enter(30, 1, pumpeth, (s,))
+
+
+s.enter(5, 1, pumpeth, (s,))
+s.run()
